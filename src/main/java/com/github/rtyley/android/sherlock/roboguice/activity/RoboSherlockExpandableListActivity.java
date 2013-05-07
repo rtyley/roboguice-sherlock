821ed80b3768d6483d738eb2614e08731c6d0015
Mario de Frutos <mario.defrutos@supertruper.com>
2013-05-07 17:08:28 +0200

    adapt expandable list to roboguice

---
+++ src/main/java/com/github/rtyley/android/sherlock/roboguice/activity/RoboSherlockExpandableListActivity.java
@@ -0,0 +1,107 @@
+package com.github.rtyley.android.sherlock.roboguice.activity;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import com.actionbarsherlock.app.SherlockExpandableListActivity;
+import com.google.inject.Key;
+import roboguice.RoboGuice;
+import roboguice.activity.event.*;
+import roboguice.event.EventManager;
+import roboguice.inject.RoboInjector;
+import roboguice.util.RoboContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RoboSherlockExpandableListActivity extends SherlockExpandableListActivity implements RoboContext {
+
+    protected EventManager eventManager;
+    protected HashMap<Key<?>, Object> scopedObjects = new HashMap<Key<?>, Object>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        final RoboInjector injector = RoboGuice.getInjector(this);
+        eventManager = injector.getInstance(EventManager.class);
+        injector.injectMembersWithoutViews(this);
+        super.onCreate(savedInstanceState);
+        eventManager.fire(new OnCreateEvent(savedInstanceState));
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        eventManager.fire(new OnRestartEvent());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        eventManager.fire(new OnStartEvent());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        eventManager.fire(new OnResumeEvent());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        eventManager.fire(new OnPauseEvent());
+    }
+
+    @Override
+    protected void onNewIntent( Intent intent ) {
+        super.onNewIntent(intent);
+        eventManager.fire(new OnNewIntentEvent());
+    }
+
+    @Override
+    protected void onStop() {
+        try {
+            eventManager.fire(new OnStopEvent());
+        } finally {
+            super.onStop();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        try {
+            eventManager.fire(new OnDestroyEvent());
+        } finally {
+            try {
+                RoboGuice.destroyInjector(this);
+            } finally {
+                super.onDestroy();
+            }
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        final Configuration currentConfig = getResources().getConfiguration();
+        super.onConfigurationChanged(newConfig);
+        eventManager.fire(new OnConfigurationChangedEvent(currentConfig, newConfig));
+    }
+
+    @Override
+    public void onContentChanged() {
+        super.onContentChanged();
+        RoboGuice.getInjector(this).injectViewMembers(this);
+        eventManager.fire(new OnContentChangedEvent());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        eventManager.fire(new OnActivityResultEvent(requestCode, resultCode, data));
+    }
+
+    @Override
+    public Map<Key<?>, Object> getScopedObjectMap() {
+        return scopedObjects;
+    }
+}
